9eea3b5c608da914db280f6b43abde483f846c4c
Aaron_Ming <aaron_ming@outlook.com>
2016-07-20 22:19:06 +0800

    20160720

---
+++ app.py
@@ -0,0 +1,24 @@
+#encoding=utf-8
+
+
+from flask import Flask, redirect, render_template, session, url_for, g
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+app = Flask(__name__)
+
+
+@app.route('/tab')
+def tab():
+	return render_template('tab.html')
+
+@app.route('/layout')
+def layout():
+	return render_template('layout.html')
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8080, debug=True)
